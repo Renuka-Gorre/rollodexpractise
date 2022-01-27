61a9b9b4c69e7aa86729f47784640e7427e101e9
Renuka Gorre <renuka.gorre@qentelli.com>
2022-01-27 22:15:06 +0530

    Rollodex app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Pages/Home/Home';
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './Pages/Home/Home'
+import './index.css'
 
 ReactDOM.render(<Home />,document.getElementById('root'))
